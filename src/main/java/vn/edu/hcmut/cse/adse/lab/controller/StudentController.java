fe9ebc15549c4932f8a4549e8c165d89a3f59307
PlayMaker1508 <nghiaphan1582k5@gmail.com>
2026-02-27 11:09:34 +0700

    Initial commit

---
+++ src/main/java/vn/edu/hcmut/cse/adse/lab/controller/StudentController.java
@@ -0,0 +1,26 @@
+package vn.edu.hcmut.cse.adse.lab.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import vn.edu.hcmut.cse.adse.lab.entity.Student;
+import vn.edu.hcmut.cse.adse.lab.service.StudentService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService service;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable String id) {
+        return service.getById(id);
+    }
+}
---
+++ src/main/java/vn/edu/hcmut/cse/adse/lab/controller/StudentWebController.java
@@ -0,0 +1,30 @@
+package vn.edu.hcmut.cse.adse.lab.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller; // Luu y: su dung @Controller, KHONG dung → @RestController
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import vn.edu.hcmut.cse.adse.lab.service.StudentService;
+import vn.edu.hcmut.cse.adse.lab.entity.Student;
+import java.util.List;
+@Controller
+@RequestMapping("/students")
+public class StudentWebController {
+    @Autowired
+    private StudentService service;
+// Route: GET http://localhost:8080/students
+@GetMapping
+public String getAllStudents(@RequestParam(required = false) String keyword, Model model) {
+List<Student> students;
+if (keyword != null && !keyword.isEmpty()) {
+// Can viet them ham searchByName trong Service/Repository
+students = service.searchByName(keyword);
+} else {
+students = service.getAll();
+}
+model.addAttribute("dsSinhVien", students);
+return "students";
+}
+}
